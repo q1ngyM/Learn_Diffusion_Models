3a753fd3c8429ba2f5abaee6e4879eb7a8bbb92e
q1ngyM <maqy23@mail2.sysu.edu.cn>
2025-03-16 17:16:15 +0800

    first commit

---
+++ utils.py
@@ -0,0 +1,36 @@
+# this script refer to `diffusion_utilities.py` from the tutorial at https://s172-29-19-168p8888.lab-aws-production.deeplearning.ai/edit/L3_Context/diffusion_utilities.py
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+class CustomDataset(Dataset):
+    def __init__(self, sfilename, lfilename, transform, null_context=False):
+        self.sprites = np.load(sfilename)
+        self.slabels = np.load(lfilename)
+        print(f"sprite shape: {self.sprites.shape}")
+        print(f"labels shape: {self.slabels.shape}")
+        self.transform = transform
+        self.null_context = null_context
+        self.sprites_shape = self.sprites.shape
+        self.slabel_shape = self.slabels.shape
+                
+    # Return the number of images in the dataset
+    def __len__(self):
+        return len(self.sprites)
+    
+    # Get the image and label at a given index
+    def __getitem__(self, idx):
+        # Return the image and label as a tuple
+        if self.transform:
+            image = self.transform(self.sprites[idx])
+            if self.null_context:
+                label = torch.tensor(0).to(torch.int64)
+            else:
+                label = torch.tensor(self.slabels[idx]).to(torch.int64)
+        return (image, label)
+
+    def getshapes(self):
+        # return shapes of data and labels
+        return self.sprites_shape, self.slabel_shape
+    
